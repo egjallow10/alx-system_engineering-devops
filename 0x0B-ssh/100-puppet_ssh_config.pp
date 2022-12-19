2d32281282a6d95cc3d97e119e46428b79bf881d
egjallow10 <egjallow10@gmail.com>
2022-12-19 08:33:23 -0800

    file changes;

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 #ssh config file
-file { 'Turn off passwordd':
+file_line { 'Turn off passwordd':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
 
-file { 'Declare identity file':
+file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school',
