3b015f41df3c805996a06d86e2864d0b5f0fdf7c
egjallo <egjallo@insistglobal.com>
2023-01-16 13:50:18 +0000

    changes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -17,19 +17,26 @@ Employee [EMPLOYEE_NAME] is done with tasks(#_DONE_TASKS/TOTAL_#_TASKS):
 
 
 if __name__ == '__main__':
+    import requests
     from requests import get
     from sys import argv
 
     try:
-        URL = "https://jsonplaceholder.typicode.com"
-        ID = argv[1]
-        employee = get(URL + "/users/" + ID).json()
-        name = employee.get('username')
-        all_tasks = get(URL + "/todos?userId=" + ID).json()
-        done_tasks = [task.get('title')
-                      for task in all_tasks if task.get('completed')]
-        print("Employee {} is done with tasks({}/{}):"
-              .format(name, len(done_tasks), len(all_tasks)))
-        print("\t", "\n\t ".join(done_tasks))
-    except IndexError:
-        print("USAGE: ./0-gather_data_from_an_API.py <NUMERIC_ID>")
+        employee_id = int(argv[1])
+        api_url = 'https://jsonplaceholder.typicode.com'
+        users = requests.get(
+            '{}/users/{}'.format(api_url, employee_id))
+        name = users.json()['name']
+        todos = requests.get(
+            '{}/todos?userId={}'.format(api_url, employee_id))
+        total_tasks = len(todos.json())
+        done_tasks = 0
+        done_titles = ""
+        for i in todos.json():
+            if i['completed'] is True:
+                done_tasks += 1
+                done_titles += "\n\t {}".format(i['title'])
+        print('Employee {} is done with tasks({}/{}):{}'.format(
+            name, done_tasks, total_tasks, done_titles))
+    except(RuntimeError, TypeError, NameError):
+        pass
